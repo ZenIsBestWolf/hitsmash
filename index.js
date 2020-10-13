d5b5c41367efe84bb7231d226fb74a769e9292b2
Zen Wolf <zenisbestwolf@gmail.com>
2020-10-13 16:15:37 -0400

    like a ton of stuff

--- index.js
+++ index.js
@@ -1,27 +1,51 @@
 const Discord = require('discord.js');
-const client = new Discord.Client({ disableMentions: "everyone", partials: ['MESSAGE', 'REACTION'] });
+const client = new Discord.Client({ disableMentions: "everyone", partials: ['MESSAGE'] });
 const prefix = "hs!";
 const token = require("./token.json");
 const stations = require("./stations.json"); // List of Stations
-const srcURL = "https://radio.streemlion.com/onlyhitjapan";
+const commands = require("./commands.json")
+const urlScheme = "https://radio.streemlion.com/";
 
 client.on('ready', () => {
     console.log("Ready")
+    client.user.setPresence({
+        activity: {
+            name: "OnlyHit STATION",
+            type: "LISTENING"
+        },
+        status: "online"
+    });
 });
 
 client.on('message', async message => {
     if (message.partial) await message.fetch(); // Deal with partials
-    if (message.channel.type == "dm") return; // fuck off
+    if (message.channel.type == "dm") return; // Don't bother with DMs, although it would be cool if bots could call you...
     if (message.author.bot) return;
-    if (message.content == prefix + "fuck") message.reply("fuck"); // test
-    if (message.content == prefix + "getin") {
-        if (message.member.voice.channel) {
-            const connection = await message.member.voice.channel.join();
-            const dispatcher = connection.play(srcURL);
-        } else {
-            message.reply("you\'re not in a channel dipshit.");
-        };
-    };
+    let args = message.content.substring(prefix.length).split(" ");
+    let command = commands[args[0].toLowerCase()]; // TODO: get rid of this if i dont use it again
+    if (command === undefined) return;
+    switch (args[0].toLowerCase()) {
+        case "station":
+            if (!message.member.voice.channel) {
+                message.reply("You need to be in a voice channel to do that!");
+                break;
+            }
+            let station = stations[args[1].toLowerCase()];
+            if (station === undefined) return;
+            message.reply("Switching station to " + station["name"]);
+            let connection = await message.member.voice.channel.join();
+            let dispatch = connection.play(urlScheme + station["url"]);
+            break;
+
+        case "stop":
+            //console.log(message.guild);
+            if (message.guild.voice.channel == undefined) message.reply("I'm not in a channel!");
+            else message.guild.voice.channel.leave();
+            break;
+
+        default:
+            break;
+    }
 });
 
 client.login(token.token);
