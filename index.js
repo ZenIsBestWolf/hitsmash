399f0bb17fbcbc14cf492b7c316912cfb015294a
Zen Wolf <zenisbestwolf@gmail.com>
2020-10-14 17:27:07 -0400

    Public functionality

--- index.js
+++ index.js
@@ -3,14 +3,14 @@ const client = new Discord.Client({ disableMentions: "everyone", partials: ['MES
 const prefix = "hs!";
 const token = require("./token.json");
 const stations = require("./stations.json"); // List of Stations
-const commands = require("./commands.json")
+const commands = require("./commands.json");
 const urlScheme = "https://radio.streemlion.com/";
 
 client.on('ready', () => {
     console.log("Ready")
     client.user.setPresence({
         activity: {
-            name: "OnlyHit STATION",
+            name: "OnlyHit US/Japan/Gold/K-Pop",
             type: "LISTENING"
         },
         status: "online"
@@ -25,25 +25,62 @@ client.on('message', async message => {
     let command = commands[args[0].toLowerCase()]; // TODO: get rid of this if i dont use it again
     if (command === undefined) return;
     switch (args[0].toLowerCase()) {
+        case "help":
+            let tbsHelp = "Here are the list of commands:\n\n";
+            for (let i=0; i < Object.keys(commands).length; i++) {
+                let localcmd = commands[Object.keys(commands)[i]]
+                if (localcmd["info"] == "alias") continue;
+                tbsHelp+="**"+prefix+Object.keys(commands)[i];
+                let tempvar = localcmd["args"];
+                if (!(tempvar === undefined)) tbsHelp+=" ["+tempvar+"]";
+                tbsHelp+="**: " + localcmd["info"] + "\n";
+            }
+            message.channel.send(tbsHelp);
+            break;
+        case "play":
         case "station":
             if (!message.member.voice.channel) {
                 message.reply("You need to be in a voice channel to do that!");
                 break;
             }
             let station = stations[args[1].toLowerCase()];
-            if (station === undefined) return;
-            message.reply("Switching station to " + station["name"]);
-            let connection = await message.member.voice.channel.join();
-            let dispatch = connection.play(urlScheme + station["url"]);
+            if (station === undefined) {
+                message.reply("You provided an invalid station. Check the list of valid stations with ``" + prefix + "stations``!");
+                return;
+            }
+            let tripwire = false; // I CANT FUCKING BREAK OR RETURN IN A CATCH STATEMENT WHYYY
+            let connection = await message.member.voice.channel.join().catch(() => {
+                message.reply("I couldn't find your voice channel!");
+                tripwire=true;
+            });
+            if (tripwire) return;
+            message.reply("Tuning to station **" + station["name"] + "**");
+            connection.play(urlScheme + station["url"]);
             break;
 
+        case "disconnect":
+        case "fuckoff":
         case "stop":
-            if (message.guild.voice.channel == undefined) message.reply("I'm not in a channel!");
-            else message.guild.voice.channel.leave();
+            try {
+                message.guild.voice.channel.leave()
+            } catch (error) {
+                message.reply("I am not in a channel!")
+            }
+            //message.guild.voice.channel.leave()
             break;
         
         case "info":
             message.reply("OnlyHit can be found here: https://onlyhit.us/. I am developed by Zen#0004 and my repository exists at https://github.com/ZenIsBestWolf/hitsmash.");
+            break;
+
+        case "invite":
+            message.reply("https://discord.com/oauth2/authorize?client_id=765364533136982067&scope=bot&permissions=34606080");
+            break;
+        
+        case "stations":
+            let tbsStations = "Here is the list of stations:\n\n";
+            for (let i=0; i<Object.keys(stations).length; i++) tbsStations+="**"+stations[Object.keys(stations)[i]]["name"] + "**: ``" + Object.keys(stations)[i] + "``\n";
+            message.channel.send(tbsStations);
             break;
 
         default:
